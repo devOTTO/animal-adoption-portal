c3b373ba5b649bd053b022386326e585e15e4953
yujinpark <petere123123@gmail.com>
2020-12-03 10:30:06 +0900

    Change Test Code & workflow

---
+++ AnimalAdoption.Web.Portal.UnitTests/LoginLogic.cs
@@ -0,0 +1,22 @@
+﻿using AnimalAdoption.Common.Logic;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace AnimalAdoption.Service.Login.UnitTests
+{
+    public class LoginTests
+    {
+        [Fact]
+        public void TestGetLoginUserFromId()
+        {
+            var loginId = "placeholder";
+
+            var memoryCache = new MemoryCache(new MemoryCacheOptions());
+            var loginService = new LoginService();
+
+            Assert.Equal("DevOps_Team3", loginService.GetLoginUserFromId(loginId))
+        }
+    }
+}
